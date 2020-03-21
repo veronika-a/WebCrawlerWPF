0e91cc2c38330d5c59b86734acf3e4103fce2a8e
veronika-a <andrianika00@gmail.com>
2020-03-21 19:54:33 +0200

    Create Search.xaml.cs

---
+++ Views/Search.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using WebCrawlerWPF.Models;
+using WebCrawlerWPF.ViewModels;
+
+namespace WebCrawlerWPF.Views
+{
+    /// <summary>
+    /// Логика взаимодействия для Search.xaml
+    /// </summary>
+    public partial class Search : Window
+    {
+        SearchViewModel viewModel;
+        public Search(ref SPage page)
+        {
+            viewModel = new SearchViewModel(page);
+            DataContext = viewModel;
+            InitializeComponent();
+
+            viewModel.Closing += (s, e) => this.Close();
+        }
+    }
+}
